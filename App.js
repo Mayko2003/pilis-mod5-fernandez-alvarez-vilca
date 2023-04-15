15cb7d11fafbf2af3a3abb891da4cb843f958835
Mayko2003 <maykox34f@gmail.com>
2023-04-14 22:33:33 -0300

    fix: add missing import

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet } from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { MainStackScreen } from './src/screens/MainStackScreen';
+import { NavigationContainer } from '@react-navigation/native';
 
 const MainStack = createNativeStackNavigator()
 
