c666a421fc57e82896449ff929c6ad8ef10c0600
Alvaro Alvarez <tecnoasys@gmail.com>
2023-04-16 23:54:13 -0300

    nuevo

--- src/api/data.js
+++ src/api/data.js
@@ -6,13 +6,9 @@ export const data = [
     rating: "4.9",
     description:
       "Escapa a esta hermosa casa de playa ubicada en las costas de Malibú. Esta casa cuenta con impresionantes vistas del océano y está completamente equipada con todas las comodidades que necesitas para una divertida y relajante escapada a la playa.",
-    images: [
-      require("../../assets/images/Pachamama-1282014073.jpg"),
-      require("../../assets/images/beach_house.webp"),
-      require("../../assets/images/beach_house.webp"),
-      require("../../assets/images/beach_house.webp"),
-      require("../../assets/images/beach_house.webp"),
-    ],
+
+    images: [require("../../assets/images/Pachamama-1282014073.jpg")],
+
     price: "$1000/noche",
   },
 ];
