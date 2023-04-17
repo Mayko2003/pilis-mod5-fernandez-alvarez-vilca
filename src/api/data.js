c11de3175e584130ce4eab3082f8c30672b34ad6
Alvaro Alvarez <tecnoasys@gmail.com>
2023-04-16 23:28:53 -0300

    nuevo

---
+++ src/api/data.js
@@ -0,0 +1,18 @@
+export const data = [
+  {
+    id: "1",
+    title: "Casa en la playa de lujo",
+    location: "Malibú, California",
+    rating: "4.9",
+    description:
+      "Escapa a esta hermosa casa de playa ubicada en las costas de Malibú. Esta casa cuenta con impresionantes vistas del océano y está completamente equipada con todas las comodidades que necesitas para una divertida y relajante escapada a la playa.",
+    images: [
+      require("../../assets/images/Pachamama-1282014073.jpg"),
+      require("../../assets/images/beach_house.webp"),
+      require("../../assets/images/beach_house.webp"),
+      require("../../assets/images/beach_house.webp"),
+      require("../../assets/images/beach_house.webp"),
+    ],
+    price: "$1000/noche",
+  },
+];
