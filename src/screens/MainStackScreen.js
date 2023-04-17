793b79be8a51dd84601a08082e09b3aae910c38b
Alvaro Alvarez <tecnoasys@gmail.com>
2023-04-16 23:29:03 -0300

    nuevo

--- src/screens/MainStackScreen.js
+++ src/screens/MainStackScreen.js
@@ -2,11 +2,27 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { HomeScreen } from "./home/HomeScreen";
 import { SearchScreen } from "./Search/SearchScreen";
 import { ProfileScreen } from "./Profile/ProfileScreen";
+import { Entypo } from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
 
+const TAB_ICON = {
+  Inicio: "home",
+  Perfil: "at-sharp",
+  Buscar: "search-circle-outline",
+};
+
 const screenOptions = ({ route }) => {
-  return {};
+  const iconName = TAB_ICON[route.name];
+  return {
+    tabBarIcon: ({ size, color }) => (
+      (<Entypo name={iconName} size={size} color={color} />),
+      (<Ionicons name={iconName} size={size} color={color} />)
+    ),
+    tabBarActiveTintColor: "blue",
+    tabBarInactiveTintColor: "gray",
+  };
 };
 
 export const MainStackScreen = () => {
@@ -15,7 +31,7 @@ export const MainStackScreen = () => {
       {/* AGREGAR RUTAS PARA LAS PANTALLAS */}
       <Tab.Screen name="Inicio" component={HomeScreen} />
       <Tab.Screen name="Buscar" component={SearchScreen} />
-      <Tab.Screen name="Usuario" component={ProfileScreen} />
+      <Tab.Screen name="Perfil" component={ProfileScreen} />
     </Tab.Navigator>
   );
 };
---
+++ src/screens/Search/SearchScreen.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import {
+  Text,
+  View,
+  SafeAreaView,
+  FlatList,
+  Pressable,
+  images,
+} from "react-native";
+import { styles } from "./SearchScreenStyles";
+import { data } from "../../api/data";
+
+const buscar = ([item]) => (
+  <Pressable>
+    <View style={styles.itemContainer}>
+      <images source={item.images[0]} styles={styles.itemImage} />
+      <Text style={styles.itemTitle}>{item.title} </Text>
+      <Text style={styles.itemPtice}>{item.price} </Text>
+    </View>
+  </Pressable>
+);
+
+export const SearchScreen = () => {
+  return (
+    <SafeAreaView styles={styles.container}>
+      <FlatList
+        data={data}
+        renderItem={buscar}
+        heyExtractor={(item) => item.id}
+        style={styles.itemList}
+      />
+    </SafeAreaView>
+  );
+};
