a87f44b63b945acd1cc7bbd68834b859b00396a4
Alvaro Alvarez <tecnoasys@gmail.com>
2023-04-16 21:06:03 -0300

    nuevo

--- App.js
+++ App.js
@@ -1,28 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet } from 'react-native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import { MainStackScreen } from './src/screens/MainStackScreen';
-import { NavigationContainer } from '@react-navigation/native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet } from "react-native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { MainStackScreen } from "./src/screens/MainStackScreen";
+import { NavigationContainer } from "@react-navigation/native";
 
-const MainStack = createNativeStackNavigator()
+const MainStack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <>
       <NavigationContainer>
-          <MainStack.Navigator screenOptions={{ headerShown: false }}>
-            {/* //Pantallas con Tab */}
-            <MainStack.Screen name='Main' component={MainStackScreen} />
-            {/* //Pantallas sin Tab */}
+        <MainStack.Navigator screenOptions={{ headerShown: false }}>
+          {/* //Pantallas con Tab */}
 
-          </MainStack.Navigator>
-        </NavigationContainer>
+          <MainStack.Screen name="Main" component={MainStackScreen} />
+          {/* //Pantallas sin Tab */}
+        </MainStack.Navigator>
+      </NavigationContainer>
       <StatusBar style="auto" />
     </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-  },
+  container: {},
 });
